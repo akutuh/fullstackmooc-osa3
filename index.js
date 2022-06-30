6d8db71d8d9dec1fdd4cc2b322eb2e2e30519262
akutuh <akutuh@uef.fi>
2022-07-01 01:10:39 +0300

    puhelinluettelo 3.3

--- index.js
+++ index.js
@@ -27,8 +27,18 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-
     response.json(notes)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 app.get('/info', (request, response) => {
