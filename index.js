5b525d911a00e59d1056702370d034b1bfafc159
akutuh <akutuh@uef.fi>
2022-07-02 04:53:46 +0300

    frontend build

--- index.js
+++ index.js
@@ -27,10 +27,11 @@ let persons = [
     }
 ]
 
-
 app.use(express.json())
 
 app.use(cors())
+
+app.use(express.static('build'))
 
 morgan.token('body', (request, response) => JSON.stringify(request.body));
     app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
